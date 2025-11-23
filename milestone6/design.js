cca2517a31b90a562978c8a01b658022a809ba68
rpm4833 <d00483366@utahtech.edu>
2025-11-22 22:33:01 -0700

    Completed milestone6

--- milestone6/design.js
+++ milestone6/design.js
@@ -1,22 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll("#phase-selector button");
-  const phases = document.querySelectorAll("#space-elevator > div[id^='phase']");
+const img = document.getElementById("design-img");
 
-  // Hide all phase sections
-  phases.forEach(p => p.style.display = "none");
+document.querySelectorAll('input[name="product"]').forEach(radio => {
+  radio.addEventListener("change", () => {
+    img.src = "designs/" + radio.value + "-design.png";
+  })
+})
 
-  buttons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      const phase = btn.dataset.phase;
-
-      // highlight button
-      buttons.forEach(b => b.classList.remove("active"));
-      btn.classList.add("active");
-
-      // show correct phase section
-      phases.forEach(p => {
-        p.style.display = p.id === "phase" + phase ? "block" : "none";
-      });
-    });
-  });
-});
